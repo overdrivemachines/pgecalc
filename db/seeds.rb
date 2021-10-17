358a191b00978398e79d4de94e4a2d372e0614ba
Dipen Chauhan <get.dipen@gmail.com>
2021-10-17 11:05:05 -0700

    Create sample properties

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Property.create(abbreviation: "407", address: "407 W 9th St, Chico, CA 95928")
+p2 = Property.create(abbreviation: "341", address: "341 W 9th St, Chico, CA 95928")
+
+# b1 = Bill.create(amount: 32, start_date: , end_date: , property_id: p1.id)
