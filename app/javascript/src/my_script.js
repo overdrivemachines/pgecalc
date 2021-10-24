65a083f03b7848c1915a4865d5bee4d019826b02
Dipen Chauhan <get.dipen@gmail.com>
2021-10-24 15:01:49 -0700

    Removed ajax:success and ajax:error

--- app/javascript/src/my_script.js
+++ app/javascript/src/my_script.js
@@ -1,11 +1,3 @@
 window.addEventListener("load", () => {
-  const element = document.querySelector("#collapse-property-new");
-  element.addEventListener("ajax:success", (event) => {
-    const [_data, _status, xhr] = event.detail;
-    console.log("***SUCCESS***");
-  });
-  element.addEventListener("ajax:error", () => {
-    // element.insertAdjacentHTML("beforeend", "<p>ERROR</p>");
-    console.log("***ERROR***");
-  });
+
 });
