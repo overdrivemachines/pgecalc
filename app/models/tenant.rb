b81fe95ae0bcb05d7e60cdcfc83f39d98c9f73db
Dipen Chauhan <get.dipen@gmail.com>
2021-10-23 22:04:18 -0700

    Added validations

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: tenants
+#
+#  id            :integer          not null, primary key
+#  move_in_date  :date
+#  move_out_date :date
+#  name          :string(30)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  property_id   :integer          not null
+#
+# Indexes
+#
+#  index_tenants_on_property_id  (property_id)
+#
+# Foreign Keys
+#
+#  property_id  (property_id => properties.id)
+#
 class Tenant < ApplicationRecord
+  validates :name, presence: true, length: 2..30
+  validates :move_in_date, presence: true
+  validates :property_id, presence: true
+  validate :move_out_date_is_greater_than_move_in_date
+
   belongs_to :property
+
+  def move_out_date_is_greater_than_move_in_date
+    if move_in_date.present? && move_out_date.present? && move_out_date < move_in_date
+      errors.add(:move_out_date, "can't be before move in date")
+    end
+  end
 end
