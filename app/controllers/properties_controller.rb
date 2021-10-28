661ab9fac471ec6216f5b4e80e3572662f308530
Dipen Chauhan <get.dipen@gmail.com>
2021-10-27 22:36:02 -0700

    Added back show action, defined update action

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: [:edit, :update, :destroy]
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
 
   # respond_to :html, only: [ :index ]
   # respond_to :js, only: [ :new, :edit]
@@ -10,52 +10,42 @@ class PropertiesController < ApplicationController
     @property = Property.new
   end
 
-  # GET /properties/new
-  def new
-    @property = Property.new
-  end
-
-  # GET /properties/1/edit
-  def edit
+  # GET /properties/1
+  def show
   end
 
   # POST /properties
   def create
-    # TODO
     @property = Property.new(property_params)
     @property.save
+
+    # If there are errors in saving the property,
+    # the js will render errors to #errors-container-property-1
     respond_to :js
 
+    # If there are no errors saving, show the index page
     if !@property.errors.any?
       redirect_to properties_url, notice: 'Property was successfully created.'
     end
-
-    # flash[:notice] = "Property was successfully created." if @property.save
-    # redirect_to properties_url
-    # respond_with(@property)
-    # if @property.save
-    #   redirect_to properties_url, notice: 'Property was successfully created.'
-    # else
-    #   @properties = Property.all
-    #   render :index
-    # end
   end
 
   # PATCH/PUT /properties/1
   def update
-    # TODO
-    if @property.update(property_params)
-      redirect_to properties_url, notice: 'Property was successfully updated.'
-    else
-      flash.keep
-      redirect_to properties_url
+    @property.update(property_params)
+    # If there are errors in updating the property,
+    # the js will render errors to #errors-container-property-1
+    respond_to :js
+
+    # If there are no errors saving, show the index page
+    if !@property.errors.any?
+      redirect_to properties_url, notice: 'Property was successfully created.'
     end
   end
 
   # DELETE /properties/1
   def destroy
     @property.destroy
-    redirect_to properties_url, notice: 'Property was successfully destroyed.'
+    redirect_to properties_url, notice: 'Property was successfully removed.'
   end
 
   private
