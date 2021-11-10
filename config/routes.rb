0cb91ae6d106ffcd48945034b1d71c1b37347546
Dipen Chauhan <get.dipen@gmail.com>
2021-11-09 20:16:46 -0800

    Nested tenants and bills under properties

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 # == Route Map
 #
 #                                   Prefix Verb   URI Pattern                                                                                       Controller#Action
-#                                    bills GET    /bills(.:format)                                                                                  bills#index
-#                                          POST   /bills(.:format)                                                                                  bills#create
-#                                 new_bill GET    /bills/new(.:format)                                                                              bills#new
-#                                edit_bill GET    /bills/:id/edit(.:format)                                                                         bills#edit
-#                                     bill GET    /bills/:id(.:format)                                                                              bills#show
-#                                          PATCH  /bills/:id(.:format)                                                                              bills#update
-#                                          PUT    /bills/:id(.:format)                                                                              bills#update
-#                                          DELETE /bills/:id(.:format)                                                                              bills#destroy
+#                         property_tenants GET    /properties/:property_id/tenants(.:format)                                                        tenants#index
+#                                          POST   /properties/:property_id/tenants(.:format)                                                        tenants#create
+#                      new_property_tenant GET    /properties/:property_id/tenants/new(.:format)                                                    tenants#new
+#                     edit_property_tenant GET    /properties/:property_id/tenants/:id/edit(.:format)                                               tenants#edit
+#                          property_tenant GET    /properties/:property_id/tenants/:id(.:format)                                                    tenants#show
+#                                          PATCH  /properties/:property_id/tenants/:id(.:format)                                                    tenants#update
+#                                          PUT    /properties/:property_id/tenants/:id(.:format)                                                    tenants#update
+#                                          DELETE /properties/:property_id/tenants/:id(.:format)                                                    tenants#destroy
+#                           property_bills GET    /properties/:property_id/bills(.:format)                                                          bills#index
+#                                          POST   /properties/:property_id/bills(.:format)                                                          bills#create
+#                        new_property_bill GET    /properties/:property_id/bills/new(.:format)                                                      bills#new
+#                       edit_property_bill GET    /properties/:property_id/bills/:id/edit(.:format)                                                 bills#edit
+#                            property_bill GET    /properties/:property_id/bills/:id(.:format)                                                      bills#show
+#                                          PATCH  /properties/:property_id/bills/:id(.:format)                                                      bills#update
+#                                          PUT    /properties/:property_id/bills/:id(.:format)                                                      bills#update
+#                                          DELETE /properties/:property_id/bills/:id(.:format)                                                      bills#destroy
 #                               properties GET    /properties(.:format)                                                                             properties#index
 #                                          POST   /properties(.:format)                                                                             properties#create
 #                             new_property GET    /properties/new(.:format)                                                                         properties#new
@@ -46,9 +54,11 @@
 #                     rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                                    active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
-  resources :tenants
-  resources :bills
-  resources :properties
+
+  resources :properties do
+    resources :tenants
+    resources :bills
+  end
 
   root "properties#index"
 end
